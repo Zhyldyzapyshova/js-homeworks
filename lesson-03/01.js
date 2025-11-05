ea7447959707715538179ebf0cd559ebcbbdd89b
medetovich <said.medetovich@gmail.com>
2025-11-05 19:40:31 +0100

    homework3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,53 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(dice1, dice2) {
+       return (dice1 + dice2) / 2;
+}
+console.log(calculateAverage())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let gold = 100 // 70
+
+  function createBuilding (name, cost) {
+    if (gold >= cost) {
+        gold = gold - cost
+        console.log(`${name} completed!`)
+    } else {
+        console.log(`${name} - not enough resources!`)
+    }
+    console.log("Rest of gold:", gold)
+}
+
+let buildingName = 'house'
+let buildingCost = 30
+createBuilding(buildingName, buildingCost)
+
+createBuilding('garage', 20)
+createBuilding('sauna', 40)
+
+function getSalary (currentGold, increment){
+    return currentGold + increment
+}
+
+gold = getSalary(gold, 34)
+console.log("balance: ", gold)
+
+gold = getSalary(gold, 44)
+console.log("balance: ", gold)*/
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,18 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(num1, num2, num3) {
+    let theLargest;
+    if ((num1 >= num2) && (num1 >= num3)) {
+        theLargest = num1;
+    } else if ((num2 >= num1) && (num2 >= num3)) {
+        theLargest = num2;
+    } else {
+        theLargest = num3;
+    }
+    return theLargest;
+}
+console.log(findLargest())
+
+
+
+
